362bbc19d57a43fce01c3fe42981c1803dcce97f
KrisztinaFoldi <krisztifoldi11@gmail.com>
2020-04-06 18:42:03 +0200

    fix(addKennel)

--- HTCCovidBE/HTCCovidBE/Controllers/KennelController.cs
+++ HTCCovidBE/HTCCovidBE/Controllers/KennelController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HTCCovidBE.DTOs;
+using HTCCovidBE.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,7 +12,28 @@ namespace HTCCovidBE.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    //[Authorize(Roles = "Admin")]
     public class KennelController : ControllerBase
     {
+        private readonly IKennelService kennelService;
+
+        public KennelController(IKennelService kennelService)
+        {
+            this.kennelService = kennelService;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ActionResult> AddKennel([FromBody] KennelDTO KennelDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await kennelService.AddKennelAsync(KennelDTO);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
     }
 }
